8c030b40bd375e80be094265fcf7e5bfcc15cccb
ronnylau <uzimaster@web.de>
2022-09-08 11:21:52 +0200

    Update record.py

--- record.py
+++ record.py
@@ -1,4 +1,15 @@
+def getSessionID(packet):
+    return packet.get_value('header').m_sessionUID.toString()
+
+
 def trackLapData(packet, racedata, carstatus):
+    lapdata = packet.get_value('m_lapData')
+    print(lapdata)
+    pass
+    sessionID = getSessionID(packet)
+    if carstatus and racedata.data and racedata.data[0] and racedata.data[0]['lap_data']:
+        # for
+        pass
     return racedata
 
 
