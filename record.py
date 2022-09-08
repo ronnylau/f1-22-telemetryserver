2114e0bd8aad2e9d5ab9c7130dd6d10d507bc123
ronnylau <uzimaster@web.de>
2022-09-08 12:58:40 +0200

    Update record.py

--- record.py
+++ record.py
@@ -9,8 +9,10 @@ def trackLapHistoryData(packet, racedata, carstatus):
     sessionID = getSessionID(lapdata)
     if carstatus:
         carstatus = carstatus.to_dict()
-    print(lapdata)
-    exit(1)
+    # try to find the best lap time
+    if lapdata.best_lap_time_lap_num > 0:
+        print(lapdata['lap_history_data'][lapdata.best_lap_time_lap_num-1]['lap_time_in_ms'])
+        exit(1)
     return racedata
 
 
