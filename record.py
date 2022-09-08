d475b0cc671f8c7837cd442265de9fa62011b5eb
ronnylau <uzimaster@web.de>
2022-09-08 11:59:02 +0200

    Update record.py

--- record.py
+++ record.py
@@ -1,14 +1,13 @@
 def getSessionID(packet):
     header = packet.to_dict().header
-    return header.sessionUID.toString()
+    return header['sessionUID'].toString()
 
 
 def trackLapData(packet, racedata, carstatus):
     lapdata = packet.to_dict()
-    print(lapdata['lap_data'])
-    exit(1)
-    pass
+    # print(lapdata['lap_data'])
     sessionID = getSessionID(packet)
+    print(sessionID)
     if carstatus and racedata.data and racedata.data[0] and racedata.data[0]['lap_data']:
         # for
         pass
