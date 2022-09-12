f99ae44150e58ecfe30432fa54132b19ce2468ef
ronnylau <uzimaster@web.de>
2022-09-12 22:45:19 +0200

    Update participants.py

--- participants.py
+++ participants.py
@@ -11,7 +11,7 @@ class Participants:
 
     def update(self, data):
         for key, value in enumerate(data):
-            self.participants[key] = Driver(value)
+            self.participants[key] = Driver(value[key])
 
     def hasparticipants(self):
         if len(self.participants) > 0:
@@ -29,6 +29,9 @@ class Participants:
             print(f'1 Write Setup data for Driver #{self.participants[key].driver_id} {self.participants[key].name}')
             print(f"2 Front Wing = {car_setups[key].get('front_wing')}")
             self.participants[key].getCar().updateSetup(car_setups[key])
+
+            if self.participants[0].getCar() == self.participants[1].getCar():
+                print("gleich")
 
             if self.participants[key].driver_id == 2:
                 print(self.participants[key].getCar().setup)
