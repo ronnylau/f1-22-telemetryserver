84a676436d73ab0058b768657188d5abc9c394e5
ronnylau <uzimaster@web.de>
2022-09-12 22:53:08 +0200

    Update participants.py

--- participants.py
+++ participants.py
@@ -25,9 +25,9 @@ class Participants:
             if self.participants[key].driver_id == 255:
                 continue
 
-            print(f"Key {key}")
-            print(f'1 Write Setup data for Driver #{self.participants[key].driver_id} {self.participants[key].name}')
-            print(f"2 Front Wing = {car_setups[key].get('front_wing')}")
+            #print(f"Key {key}")
+            #print(f'1 Write Setup data for Driver #{self.participants[key].driver_id} {self.participants[key].name}')
+            #print(f"2 Front Wing = {car_setups[key].get('front_wing')}")
             self.participants[key].getCar().updateSetup(car_setups[key])
 
             if self.participants[0].getCar() == self.participants[1].getCar():
