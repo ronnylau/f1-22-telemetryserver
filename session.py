4bace8a8d79133daa982374178c6b78ba0698d9f
ronnylau <uzimaster@web.de>
2022-09-12 11:30:19 +0200

    Update session.py

--- session.py
+++ session.py
@@ -48,4 +48,5 @@ class Gamesession:
     def updateCarSetups(self, car_setups):
         self.participants.updateSetups(car_setups)
 
-        print(self.participants)
+        for index, value in enumerate(self.participants):
+            print(self.participants[index].getCar())
