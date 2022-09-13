817e6f6a3e3d6e06e0be59ea21995db8def3eb66
ronnylau <uzimaster@web.de>
2022-09-13 11:27:53 +0200

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,36 +1,5 @@
 import json
 
-
-class Car:
-    setup = {}
-    name = 'foo'
-
-    def __int__(self):
-        self.motion = None
-        self.lap = None
-        self.setup = {}
-        self.telemetry = None
-        self.status = None
-        self.damage = None
-        self.laphistory = None
-
-    def updateSetup(self, setup):
-        print('aufruf von Car.updatesetup mit Daten:')
-        print(setup)
-        print('beginn for schleife in Car.updatesetup')
-        for key, value in enumerate(setup):
-            print(f'key = {key} value={value}')
-            print(f'setze neuen wert {value} auf {setup.get(value)}')
-            self.setup[value] = setup.get(value)
-
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
-
-    def __str__(self):
-        return self.toJSON()
-
-
 class Driver:
     driver_id = 0
     network_id = 0
@@ -39,13 +8,41 @@ class Driver:
     name = ""
     car = None
 
+    class Car:
+        setup = {}
+        name = 'foo'
+
+        def __int__(self):
+            self.motion = None
+            self.lap = None
+            self.setup = {}
+            self.telemetry = None
+            self.status = None
+            self.damage = None
+            self.laphistory = None
+
+        def updateSetup(self, setup):
+            print('aufruf von Car.updatesetup mit Daten:')
+            print(setup)
+            print('beginn for schleife in Car.updatesetup')
+            for key, value in enumerate(setup):
+                print(f'key = {key} value={value}')
+                print(f'setze neuen wert {value} auf {setup.get(value)}')
+                self.setup[value] = setup.get(value)
+
+        def toJSON(self):
+            return json.dumps(self, default=lambda o: o.__dict__,
+                              sort_keys=True, indent=4)
+
+        def __str__(self):
+            return self.toJSON()
     def __init__(self, driver):
         self.driver_id = driver['driver_id']
         self.network_id = driver['network_id']
         self.team_id = driver['team_id']
         self.race_number = driver['race_number']
         self.name = driver['name']
-        self.car = Car()
+        self.car = self.Car()
 
     def getCar(self):
         return self.car
