26ce84f073f42e4409638a78d3bb2dd6b577ed6e
ronnylau <uzimaster@web.de>
2022-09-14 21:37:31 +0200

    Revert "Update driver.py"
    This reverts commit e46b61961de442209703e6b018350ff1998af1e1.

--- driver.py
+++ driver.py
@@ -37,7 +37,7 @@ class Driver:
         def updateDamage(self, damage):
             diff = DictDiffer(self.damage, damage)
             for key, index in enumerate(diff.changed()):
-                self.damage[index] = damage[index]
+                self.damage[key] = damage[key]
 
         def toJSON(self):
             return json.dumps(self, default=lambda o: o.__dict__,
