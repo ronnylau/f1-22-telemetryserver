5082ecc325b6588d206e8c3b469a415f0b8e1427
ronnylau <uzimaster@web.de>
2022-09-14 21:36:52 +0200

    Revert "Update handler.py"
    This reverts commit 4573cd9e88e277e2c9ba786c324622b80106fc2e.

--- handler.py
+++ handler.py
@@ -18,5 +18,6 @@ class PacketHandler:
             handler = getattr(self, f"handle_{name}", None)
             print(name)
             print(handler)
-            if handler is not None:
-                handler(packet)
+            # if handler is not None:
+            #    handler(packet)
+            handler(packet)
