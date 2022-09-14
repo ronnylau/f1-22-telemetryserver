1fb24f45944a4126920de284a5feea169b090a1b
ronnylau <uzimaster@web.de>
2022-09-14 21:37:36 +0200

    Revert "Update main.py"
    This reverts commit 2d4a50c08b423987ee537003249e9844a686bf8e.

--- main.py
+++ main.py
@@ -191,10 +191,10 @@ def main():
                         result.write(str(session))
                         result.write('\nDrivers:\n')
                         # list all priticipants
-                        if len(session.getparticipants()) > 0:
+                        if session.getparticipants().hasparticipants():
                             participantList = session.getparticipants()
-                            for key, driver in enumerate(participantList):
-                                participant = participantList[key]
+                            for key, driver in enumerate(participantList.participants):
+                                participant = participantList.participants[key]
                                 result.write(str(participant) + '\n')
                                 result.write('\t\t' + str(participant.getCar().setup) + '\n')
                     log.write('\nPacketFinalClassificationData\n')
