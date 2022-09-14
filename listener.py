7b00d8c87cacc7455aa205fa0a49645969206806
ronnylau <uzimaster@web.de>
2022-09-14 21:52:23 +0200

    Revert "update"
    This reverts commit 5232c9acddc6b4a62a06c397c32f3ca02d1cdab8.

--- listener.py
+++ listener.py
@@ -2,26 +2,27 @@
 Basic listener to read the UDP packet and convert it to a known packet format.
 """
 
-import platform
 import socket
+from packets import PacketHeader, HEADER_FIELD_TO_PACKET_TYPE
 
-from packets import resolve
 
+class TelemetryListener:
+    def __init__(self, host: str = None, port: int = None):
 
-class PacketListener:
-    def __init__(self, host: str = "", port: int = 20777):
+        # Set to default port used by the game in telemetry setup.
+        if not port:
+            port = 20777
+
+        if not host:
+            host = ''
+
         self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        if platform.system() == "Windows":
-            self.socket.settimeout(0.5)
         self.socket.bind((host, port))
 
     def get(self):
-        while True:
-            try:
-                return resolve(self.socket.recv(2048))
-            except socket.timeout:
-                pass
+        packet = self.socket.recv(2048)
+        header = PacketHeader.from_buffer_copy(packet)
 
-    def __iter__(self):
-        while True:
-            yield self.get()
+        key = (header.packet_format, header.packet_version, header.packet_id)
+
+        return HEADER_FIELD_TO_PACKET_TYPE[key].unpack(packet)
